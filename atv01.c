315619d4607145fd8d9b7e6397b322e28b6333e4
alef <alefvianabarroso01@gmail.com>
2026-04-02 17:50:14 -0400

    teste-02

--- atv01.c
+++ atv01.c
@@ -1,7 +1,19 @@
 # include <stdio.h>
-
-int main()
+int main(
+)
 {
+    /*declaração de variável*/
+    int myNum = 15;
+    
+    /* A impressão de de mensagem */
     printf("Hello, World!\n");
+    printf("I am learning C!\n");
+    printf("And it is awesome!\n");
+    printf(":]\n");
+    printf("myNum: %d\n", myNum);/*A impressão da variável*/
+
+    /* fim de primeiro parte*/
     return 0;
 }
+    /*Teste de atividade 01 - hello world
+    comit de primeiro projeto em liguagem C */
