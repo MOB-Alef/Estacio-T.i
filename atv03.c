d76f94b8a4f3f75165ed8b606df86576d644cd62
alef <alefvianabarroso01@gmail.com>
2026-04-04 14:30:47 -0400

    Tipos de dados em C

---
+++ atv03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+/*Tipos de Dados em C*/
+int myNum = 15;/*Inteiro*/
+float myFloatNum = 5.99;/*Ponto Flutuante*/
+char myLetter = 'D';/*Caractere*/
+
+/*impressão dos valores*/
+printf("Inteiro: %d \n", myNum);
+printf("Ponto Flutuante: %f \n", myFloatNum);
+printf("Caractere: %c \n", myLetter);
+
+    /* fim do programa */
+    return 0;
+}
