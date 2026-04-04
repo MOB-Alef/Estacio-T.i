196b7dbfb429421ffb9b2cc0845e22de8343c11a
alef <alefvianabarroso01@gmail.com>
2026-04-04 15:50:11 -0400

    Tipos de dados Numerico em C

--- atv03.c
+++ atv03.c
@@ -3,14 +3,27 @@
 int main()
 {
 /*Tipos de Dados em C*/
-int myNum = 15;/*Inteiro*/
-float myFloatNum = 5.99;/*Ponto Flutuante*/
-char myLetter = 'D';/*Caractere*/
+char myGrande = 'A';/*Caractere*/
+char a = 65, b = 66, c=67;
+char myText[] = "Hello World";
+
+/*Tipo de dados Numéricos*/
+int myNum = 100;
 
 /*impressão dos valores*/
-printf("Inteiro: %d \n", myNum);
-printf("Ponto Flutuante: %f \n", myFloatNum);
-printf("Caractere: %c \n", myLetter);
+printf("Caractere: %c \n", myGrande);
+printf("Caracteres: %c %c %c\n", a, b, c);/*impressão dos valores resumida em linhas de codigo*/
+
+/*impressão individual dos valores*/
+printf("Caractere A: %c\n", a);
+printf("Caractere B: %c\n", b);
+printf("Caractere C: %c\n", c);
+
+/*impressão do número inteiro*/
+printf("Numero: %d \n", myNum);
+
+/*impressão da string*/
+printf("String: %s \n" , myText);
 
     /* fim do programa */
     return 0;
