fcf0698e697ec115510a0834b08ca974b7a21a24
alef <alefvianabarroso01@gmail.com>
2026-04-04 12:25:23 -0400

    variavel-teste

---
+++ atv02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    /*declaração de variável*/
+    int myNum = 15;
+    float myfloatNum = 5.99;
+    char myLetter = 'D';
+    
+    /*impresao da Varialves*/
+    printf("My favorite number: %d \n", myNum);
+    printf("My favorite float number: %f \n", myfloatNum);
+    printf("My favorite Letter: %c \n", myLetter);
+
+    /* fim do programa */
+    return 0;
+}
