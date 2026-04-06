725913aa8d9926e8c019eee1282b1c9a712a4747
alef <alefvianabarroso01@gmail.com>
2026-04-06 18:49:37 -0400

    Mudança na primeira letra de cada impresão e nos cometarios e Adição de exemplo estentido

--- atv03.c
+++ atv03.c
@@ -38,58 +38,81 @@ float cost_per_item = 9.99;
 float total_cost = items * cost_per_item;
 char currency = '$';
 
-/*impressão dos valores*/
+/*Exemplo estendido*/
+int normalInt = 1000;
+double normalDouble = 3.14;
 
-printf("\n**impressao dos valores**\n\n");
+short int small = -100;
+unsigned int count =25;
+long int big = 1234567890;
+long long int verybig = 9223372036854775807;
+unsigned long long int huge = 18446744073709551615U;
+long double precise =  3.141592653589793238462643383279L;
+
+/*Impressão Dos Valores*/
+
+printf("\n**Impressao Dos Valores**\n\n");
 printf("Caractere: %c \n", myGrande);
 printf("Caracteres: %c %c %c\n\n", a, b, c);/*impressão dos valores resumida em linhas de codigo*/
 
-/*impressão individual dos valores*/
+/*Impressão Individual Dos Valores*/
 
-printf("**impressao individual dos valores**\n\n");
+printf("**Impressao Individual Dos Valores**\n\n");
 printf("Caractere A: %c\n", a);
 printf("Caractere B: %c\n", b);
 printf("Caractere C: %c\n\n", c);
 
-/*impressão do número inteiro*/
+/*Impressão do Número Inteiro*/
 
-printf("**impressao do numero inteiro**\n\n");
+printf("**Impressao Do Numero Inteiro**\n\n");
 printf("Numero: %d \n", myNum);
 printf("Numero Real: %f \n",myNum2);
 printf("Numero Double: %f \n\n",myNum3);
 
-/*impressão dos números científicos*/
+/*Impressão dos Números Científicos*/
 
-printf("**impressao dos numeros cientificos**\n\n");
+printf("**Impressao Dos Numeros Cientificos**\n\n");
 printf("Numero Cientifico Float: %f \n", f1);
 printf("Numero Cientifico Double: %f \n\n", d1);
 
-/*Impressao da Presisao*/
+/*Impressao da Precisao*/
 
-printf("**impressao da precisao**\n\n");
+printf("**Impressao Da Precisao**\n\n");
 printf("Numero com precisao padrao: %f\n", myFloatNum); /*Default will show 6 digits after the decimal point*/
 printf("Numero com 1 digito: %.1f\n", myFloatNum);/*only show 1 digit*/
 printf("Numero com 2 digitos: %.2f\n", myFloatNum);/*only show 2 digits*/
 printf("Numero com 3 digitos: %.3f\n\n", myFloatNum);/*only show 4 digits*/ 
 
 
-/*impressão da string*/
+/*Impressão da String*/
 
-printf("**impressao da string**\n\n");
+printf("**Impressao Da String**\n\n");
 printf("String: %s \n\n" , myText);
 
 /*Tamanho da memória*/
-printf("**tamanho da memoria**\n\n");
-printf("tamanho de myInt: %zu bytes\n", sizeof(myInt));/*4 bytes*/
-printf("tamanho de myFloat: %zu bytes\n", sizeof(myFloat));/*4 bytes*/
-printf("tamanho de myDouble: %zu bytes\n", sizeof(myDouble));/*8 bytes*/
-printf("tamanho de myChar: %zu bytes\n", sizeof(myChar));/*1 byte*/
+printf("**Tamanho Da Memoria**\n\n");
+printf("Tamanho de myInt: %zu bytes\n", sizeof(myInt));/*4 bytes*/
+printf("Tamanho de myFloat: %zu bytes\n", sizeof(myFloat));/*4 bytes*/
+printf("Tamanho de myDouble: %zu bytes\n", sizeof(myDouble));/*8 bytes*/
+printf("Tamanho de myChar: %zu bytes\n\n", sizeof(myChar));/*1 byte*/
 
 /*Exemplo da Vida Real*/
-printf("**exemplo da vida real**\n\n");
+printf("**Exemplo da Vida Real**\n\n");
 printf("Numero de itens: %d\n", items);
-printf("custo por item: %.2f\n", cost_per_item, currency);
+printf("Custo por item: %.2f\n", cost_per_item, currency);
 printf("Valor Total: %.2f %c\n\n", total_cost, currency);
+
+/*Exemplo estendido*/
+printf("**Exemplo Estendido**\n\n");
+printf("Normal Int: %d\n", normalInt);
+printf("Normal Double: %f\n", normalDouble);
+printf("Small: %f\n", normalDouble);
+printf("Count: %u\n", count);
+printf("Big: %ld\n", big);
+printf("VeryBig: %lld\n", verybig);
+printf("Huge: %llu\n", huge);
+printf("Precise: %.30lf\n\n", precise);
+
 
     /* fim do programa */
     return 0;
