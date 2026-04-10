cec4d7d47cf01ca68173b4a192b32cc18acc159e
alef <alefvianabarroso01@gmail.com>
2026-04-10 15:05:59 -0400

    Adição atv05.c

---
+++ atv05.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+/*Conversão do Tipo C*/
+int myInt = 9.99;/*Output 9*/;
+float sum = 5 / 2;/*Output 2.000000*/;
+
+/*impressão de Valores*/
+printf("\nConversao do Tipo C - Valor: \n\n");/* Output 9*/
+printf("Conversao  :%d\n", myInt);/* Output 9*/
+printf("Soma  :%f\n\n", sum);/* Output 2.000000*/
+
+}
