31dcfc2da6119a08b393e19b11502bf8d69a16d9
alef <alefvianabarroso01@gmail.com>
2026-04-10 18:07:03 -0400

    Aritmética  Operadores (+,-,*,/,%,)

--- atv06.c
+++ atv06.c
@@ -13,12 +13,26 @@ int main(){
     int x = 10;
     int y = 3;
 
+    /*Extra*/
 
+    int z = 5;
+    z++; /*Incremento: z agora é 6*/
+    
+    
     /*Operadores de Atribuição*/
+
+    printf("\n-----Impressao de Valores-----\n\n");
     printf("Valor da Soma: %d\n", x + y); // Output: 13
-    printf("Valor da Subtração: %d\n", x - y); // Output: 7
-    printf("Valor da Multiplicação: %d\n", x * y); // Output: 30
-    printf("Valor da Divisão: %d\n", x / y); // Output: 3
-    printf("Valor do Resto: %d\n", x % y); // Output: 1
+    printf("Valor da Subtracao: %d\n", x - y); // Output: 7
+    printf("Valor da Multiplicacao: %d\n", x * y); // Output: 30
+    printf("Valor da Divisao: %d\n", x / y); // Output: 3
+    printf("Valor do Resto: %d\n\n", x % y); // Output: 1
+    
+    /*Extra*/
+    printf("-----Exemplo extra-----\n\n");
+    printf("Valor de z: %d\n", z); // Output: 6
+
+    z--; /*Decremento: z agora é 5 novamente*/
+    printf("Valor de z apos incremento: %d\n\n", z); // Output: 6
 
 }
