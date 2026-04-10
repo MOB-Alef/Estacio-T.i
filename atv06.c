61f7251daf5517f1a67bece26a4fb5a478d32ba9
alef <alefvianabarroso01@gmail.com>
2026-04-10 17:20:35 -0400

    Operadores (+,-,*,/,%,)

--- atv06.c
+++ atv06.c
@@ -9,6 +9,16 @@ int main(){
     int sum2 = sum1 + 250; /*400 (150 + 250)*/
     int sum3 = sum2 + sum2; /*800 (400 + 400)*/
 
+    /*Aritméticos*/
+    int x = 10;
+    int y = 3;
 
+
+    /*Operadores de Atribuição*/
+    printf("Valor da Soma: %d\n", x + y); // Output: 13
+    printf("Valor da Subtração: %d\n", x - y); // Output: 7
+    printf("Valor da Multiplicação: %d\n", x * y); // Output: 30
+    printf("Valor da Divisão: %d\n", x / y); // Output: 3
+    printf("Valor do Resto: %d\n", x % y); // Output: 1
 
 }
