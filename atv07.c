59e7cb2ea30e6dca566ab5ce53d00b111b88df36
alef <alefvianabarroso01@gmail.com>
2026-04-14 16:04:55 -0400

    Booleanas Variavies E Comparador

--- atv07.c
+++ atv07.c
@@ -2,10 +2,36 @@
 #include<stdbool.h>
 
 int main(){
-    /* C Booleans */
-    
+    /* C Booleans variavies */
+    bool isProgrammingFun = true;
+    bool isFishTasty = false;
+    bool isHamburgerTasty = true;
+    bool isPizzaTasty = true;
 
+    /*Exemplo*/
+    int x = 10;
+    int y = 9;
+
+
+    /* Print the values */
+    printf("\n------Impressao de variaveis booleanas em C:------\n");
+    printf("isProgrammingFun: %d\n", isProgrammingFun);
+    printf("isFishTasty: %d\n", isFishTasty);
+
+    /*Comparando valores e Variaveis*/
+    printf("\n------Comparando valores booleanos:------\n");
+    printf("Valor de x > y: %d\n\n", x > y); /* Imprime 1 (verdadeiro) se x for maior que y, caso contrário imprime 0 (falso) */
+
+    /*Exemplo de igual*/
+    printf("\n------Exemplo de igualdade:------\n");
+    printf("Valor de x == y: %d\n", 10 == 10); /* Imprime 1 (verdadeiro) se x for igual a y, caso contrário imprime 0 (falso) */
+
+    /*Lanchonete*/
+    printf("\n------Lanchonete:------\n");
+    printf("isHamburgerTasty: %d\n", isHamburgerTasty);
+    printf("isPizzaTasty: %d\n\n", isPizzaTasty);
+
+    return 0;
 
 
 }
-
