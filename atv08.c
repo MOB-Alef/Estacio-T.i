88fb3f966b8f80e37cfc701009c78e7acbb16e70
alef <alefvianabarroso01@gmail.com>
2026-04-18 14:00:04 -0400

    Adição do if else

--- atv08.c
+++ atv08.c
@@ -3,13 +3,40 @@
 
 /*{gcc atv08.c -o atv08} <-Comado para Compilar e salva as ultima Alteração */
 /*{ .\atv08 } Comando para executar o programa */
+
+/*Usa if...Else*/
 int main(){
+/*Variaveis if */
+int x = 20;
+int y = 18;
+
+/*Variaveis Else*/
+int time = 16;
+
+/*if bool*/
+bool isGreater = x > y;
+
     /*Condições e Instruções If em C */
    /*
    Operadores de Comparação */
-    if (20 > 18)
+    if (20 > 18);
     {
-        printf("20 é maior que 18\n");
+        printf("20 e maior que 18\n");
     }
+   /*
+   Operadores de Comparação 2º Opção*/
+    if (isGreater);
+    {
+        printf("20 e maior que 18\n");
+    }
+/*if Else exemplo */
+if (time < 20)
+{
+    printf("Bom dia.");
+} else{
+    printf("Boa Tarde.");
+}
+
+
 
 }/*hello hello hello hello hiiii!!!! :]*/
