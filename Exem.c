017d62522471432cc5cc68dd2214af3c93110140
ALEF VIANA BARROSO <202601516761@alunos.estacio.br>
2026-04-29 10:36:55 -0300

    Adição do positivos negativo e nulo

--- Exem.c
+++ Exem.c
@@ -11,11 +11,17 @@ int main(
 
     printf("Digite um numero: ");
     scanf("%d,",&num);
-    if (num % 2 == 0 )
+    
+    if (num > 0 )
     {
-        printf("%d e par", num);
-    } else {
-        printf("%d e impar", num);
+        printf("\n----Empresao do Positivo----\n");
+        printf("%d E postivo\n\n", num);
+    } else if (num < 0 ){
+        printf("\n----Empresao do Negativo----\n");
+        printf("%d E negativo\n\n", num);
+    }else {
+        printf("\n----Empresao do Nulo----\n");
+        printf("%d E nulo\n\n");
     }
     return 0;
 
